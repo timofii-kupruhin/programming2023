555ec02d64c0263f35fd99d7c0ba6cdb169467e2
timofii-kupruhin <timakupr@knu.ua>
2023-12-08 14:27:13 +0200

    08.12.2023

--- zachet.py
+++ zachet.py
@@ -1,10 +1,38 @@
 # Залік з програмування Купрюхін Тимофій КІ-2
 
+class Programm():
+	def __init__(self, file_name):
+		self.__FILE_NAME = file_name
 
-def run(arg):
-	return arg
+	def read_file(self):
+		with open(self.__FILE_NAME, "r") as f:
+			file_data = f.readlines()
+		return file_data
 
+	def collect_data(self):
+		user_input = input("Write your word (!q-quit): ")
+		if user_input == "!q":
+			exit()
+		return user_input
+
+	def search_user_input(self):
+		file_data = self.read_file()
+		user_input = self.collect_data()
+		result = []
+		if file_data:
+			for line in file_data:
+				if user_input in line:
+					result += [file_data.index(line)]
+		return result
+
+prog = Programm("test.txt")
 
 
 if __name__ == "__main__":
-	run()
+	while 1:
+		indexes_lst = prog.search_user_input()
+		if indexes_lst:
+			print("Your word was found in: " + ", ".join([str(index+1) for index in indexes_lst]) + " lines")
+		else:
+			print("Your word was not found !!")
+		print()
